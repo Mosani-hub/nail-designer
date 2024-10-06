962f7effbb41fb60507db0f9b5193feff81015da
Mosani-hub <mosaniel007@gmail.com>
2024-10-05 21:38:52 -0300

    subir os arquivos do meu site

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simples interação de feedback ao enviar formulário
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previne o envio real
+    const name = document.getElementById("name").value;
+    alert(`Obrigado por sua mensagem, ${name}! Entraremos em contato em breve.`);
+    document.querySelector("form").reset(); // Reseta o formulário
+});
